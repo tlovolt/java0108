96426b89048c78324b9754fe1272d9b986193074
admin <admin@DESKTOP-40100>
2020-01-09 14:04:53 +0900

    0109_1

--- java0108/src/study/Main.java
+++ java0108/src/study/Main.java
@@ -14,14 +14,15 @@ public class Main {
 		
 		//키보드로 부터 입력받기 위한 객체 생성
 		Scanner sc = new Scanner(System.in);
+		
+		//게시판 작업을 수행할 인스턴스를 생성
+		BoardService boardService = new BoardService();
+		
 		while(true) {
 			System.out.println(
 				"1.전체보기 2.상세보기 3.삽입하기 4.수정하기 5.삭제하기 6.종료");
 			System.out.print("메뉴 입력:");
 			int menu = sc.nextInt();
-			
-			//게시판 작업을 수행할 인스턴스를 생성
-			BoardService boardService = new BoardService();
 			
 			//menu에 따른 분기
 			switch(menu) {
@@ -39,22 +40,79 @@ public class Main {
 				}
 				break;
 			case 상세보기:
-				System.out.println("상세보기 선택");
+				//글번호를 입력받아서 상세보기 메소드를 호출
+				//상세보기는 글번호에 해당하는 데이터가 있으면 데이터가 리턴되고 없으면 null 
+				
+				//글번호 입력
+				System.out.print("상세보기 할 글번호:");
+				int num = sc.nextInt();
+				
+				Board board1 = boardService.getBoard(num);
+				if(board1 == null) {
+					System.out.println("글번호에 해당하는 데이터가 없습니다.");
+				}else {
+					System.out.println(board1);
+				}
+				
 				break;
 			case 삽입하기:
 				//삽입하기는 BoardService 클래스의 insertBoard가 처리
 				//인스턴스 메소드이고 매개변수는 Board 이고 리턴 타입은 boolean 입니다.
 				Board board = new Board();
 				
-				boolean r = boardService.insertBoard(board);
+				System.out.print("제목 입력:");
+				//이전에 정수 입력을 받고 엔터가 남아 있어서 이 라인이 엔터를 입력받고 넘어갑니다.
+				sc.nextLine();
+				String temp = sc.nextLine();
+				board.setTitle(temp);
 				
-				System.out.println("삽입하기 선택");
+				System.out.print("내용 입력:");
+				temp = sc.nextLine();
+				board.setContent(temp);
+				
+				boolean r = boardService.insertBoard(board);
+
 				break;
 			case 수정하기:
-				System.out.println("수정하기 선택");
+				//수정할 글번호를 입력받기
+				System.out.print("수정할 글번호:");
+				int n = sc.nextInt();
+				//글번호가 n인 데이터 찾기
+				Board b = boardService.getBoard(n);
+				if(b == null) {
+					System.out.println("글 번호에 해당하는 데이터가 없습니다.");
+					continue;
+				}else {
+					System.out.print("수정할 제목:");
+					sc.nextLine();
+					b.setTitle(sc.nextLine());
+					System.out.print("수정할 내용:");
+					b.setContent(sc.nextLine());
+					
+					boardService.updateBoard(b);
+				}
+				
 				break;
 			case 삭제하기:
-				System.out.println("삭제하기 선택");
+				//삭제할 글번호를 입력받고
+				//해당하는 데이터가 있는지 확인하고
+				//정말로 삭제할 것인지 묻고 삭제
+				
+				System.out.print("삭제할 글번호:");
+				int x = sc.nextInt();
+				
+				Board y = boardService.getBoard(x);
+				if(y == null) {
+					System.out.println("글번호에 해당하는 글이 없습니다.");
+					continue;
+				}else {
+					System.out.print("정말로 삭제(삭제는: 1 그이외는 삭제안됨):");
+					int del = sc.nextInt();
+					if(del == 1) {
+						boardService.deleteBoard(x);
+					}
+				}
+				
 				break;
 			case 종료:
 				sc.close();
